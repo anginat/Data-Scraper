ec35c44ed4064856563c216e8742459d9300cf50
Sunny M <sunny.compsci@gmail.com>
2018-06-28 21:04:56 +0530

    changed data from dict to list

--- webscrape.py
+++ webscrape.py
@@ -13,7 +13,7 @@ import tqdm
 start = time.clock()
 
 #Create and configure logger
-logging.basicConfig(filename="webscrape_py_log.log", format='%(asctime)s %(message)s', filemode='w+')
+logging.basicConfig(filename="webscrape_py_log.log", format='%(asctime)s %(message)s', filemode='a')
 #Creating an object
 logger=logging.getLogger()
 #Setting the threshold of logger to DEBUG
@@ -21,7 +21,7 @@ logger.setLevel(logging.DEBUG)
 
 try:
 	# Global object "data" to store JSON data
-	#data = []
+	#data = {}
 	
 	# Utility function to create new JSON file locally
 	def create_JSON_file(file_path, data):
@@ -36,14 +36,14 @@ try:
 			print("File object closed for: "+file_path)
 		
 	# Web Scraper funtion to scrape data for defined webpage link
-	def web_scraper(data, page_no):
-		for i in range(page_no):
+	def web_scraper(data, product_link, start_index, end_index):
+		for i in range(start_index, end_index):
 			print("-------------------------------------------------------------------------------")
 			logger.info("-------------------------------------------------------------------------------")
 			pbar = tqdm.tqdm(total=100)
 			loop_start_time = time.clock()
 			# specify the url
-			webpage_link = "https://www.myntra.com/web/v2/search/data/men-jeans?f=&p="+str(i+1)+"&rows=48"
+			webpage_link = "https://www.myntra.com/web/v2/search/data/"+product_link+"?f=&p="+str(i+1)+"&rows=48"
 			print("Web page link created: "+str(webpage_link))
 			logger.info("Web page link created: "+str(webpage_link))
 			# query the website and return the html to the variable ‘webpage’
@@ -54,12 +54,13 @@ try:
 			response_webpage = urlopen(req).read().decode('utf-8')
 			response_webpage = json.loads(response_webpage)
 			products = response_webpage["data"]["results"]["products"]
-			#print("Products JSON created for page: "+str(i+1))
-			#logger.info("Products JSON created for page: "+str(i+1))
+			print("Products JSON created for page: "+str(i+1))
+			logger.info("Products JSON created for page: "+str(i+1))
 			if products:
 				#print("Products added to list for page: "+str(i+1))
 				#logger.info("Products added to list for page: "+str(i+1))
 				#pbar2 = tqdm.tqdm(total=len(products))
+				#data.update(response_webpage)
 				for j in range(len(products)):
 					style_id = products[j]['styleid']
 					best_price_link = "https://www.myntra.com/web/offers/"+str(style_id)
@@ -69,7 +70,7 @@ try:
 					products[j].update(response_price_json)
 					#print(data[i]["bestPrice"]["price"]["discounted"])
 				#pbar2.update()
-				data.update(response_webpage)
+				data.append(response_webpage)
 				print("All Data including Best Price for "+str(len(products))+" products added to main data list for page: "+str(i+1))
 				logger.info("All Data including Best Price for "+str(len(products))+" products added to main data list for page: "+str(i+1))
 				#print(i+1)
@@ -79,32 +80,46 @@ try:
 				break
 			#print("All Products updated for page: "+str(i+1))
 			print("Execution time taken for above loop = "+str(time.clock() - loop_start_time)+" seconds")
-			pbar.update()
+			pbar.update(100)
 			print("-------------------------------------------------------------------------------")
-		return data
+		#return data
 	
 	# Auxiliary function for ThreadPool (Not in Use)
 	def foo(data):
 		return web_scraper(data, range(4))
 	
 	# Main Driver function
-	if __name__ == '__main__':
+	def main():
 		
 		manager = multiprocessing.Manager()
-		results = manager.dict()
-		
+		results = manager.list()
 		processes = []
-		page_no = [0,1,2,3,4]
 		cpus = multiprocessing.cpu_count()
-		for cpu in range(cpus-2):
-			process = multiprocessing.Process(target=web_scraper, args=(results, cpu))
+		
+		#api_prefix_link = "https://www.myntra.com/web/v2/search/data/"
+		product_link = ["mens-jeans"]
+		product_loop_limit = [2]
+		start_index = 0
+		end_index = (product_loop_limit[0] // cpus) + 1
+		
+		for cpu in range(cpus):
+			process = multiprocessing.Process(target=web_scraper, args=(results, product_link[0], start_index, end_index))
 			process.start()
+			print("Process Name - "+format(process.name))
 			processes.append(process)
+			start_index = end_index
+			end_index += end_index
+			if start_index >= product_loop_limit[0]:
+				print("start_index ========= "+format(start_index))
+				break
 		
 		for process in processes:
+			print("Process joining ==== "+format(process))
 			process.join()
 		
-		
+		#web_scraper(product_link[0], 0, 2)
+		print("Length of products is ----------------> "+format(len(results[0]["data"]["results"]["products"])))
+		print("Length of products is ----------------> "+format(len(results[1]["data"]["results"]["products"])))
 		'''
 		pool = ThreadPool(4)
 		data = {}
@@ -116,19 +131,22 @@ try:
 		results = web_scraper(data)
 		'''
 		
-		file_path = 'C:/Users/Alpha/Desktop/myntra_jeans_2.json'
+		file_path = 'C:/Users/Alpha/Desktop/'+product_link[0]+'.json'
 		create_JSON_file(file_path, results.copy())
 		#print(data[0]["data"]["results"]["products"][0]["bestPrice"]["price"]["discounted"])
 		print("********************* Execution Successfully Finished *************************")
 		print("Execution time: "+str(time.clock() - start)+" seconds")
 		
-		f = open("C:/Users/Alpha/Desktop/myntra_jeans_2.json", "r")
+		f = open("C:/Users/Alpha/Desktop/"+product_link[0]+".json", "r")
 		f_data = f.read()
 		json_data = json.loads(f_data)
 		df = pandas.DataFrame([json_data])
 		df.to_clipboard(index=False,header=False)
 		print("Copied to clipboard")
 		#print(format(json_data)[0]["data"]["results"]["products"][0]["bestPrice"]["price"]["discounted"])
+	
+	if __name__ == '__main__':
+		main()
 		
 except Exception as e:
 	print("Exception occurred: "+str(e))
